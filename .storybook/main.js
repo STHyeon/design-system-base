0977be0483325d42c32c4bd29bb9fa75d0f984ec
STHyeon <toby2009@naver.com>
2021-03-03 10:11:46 +0900

    [Add] 웹 접근성 체크 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const custom = require('../config-overrides');
 
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-a11y'],
     webpackFinal: (storybookConfig) => {
         const customConfig = custom(storybookConfig);
         return {
